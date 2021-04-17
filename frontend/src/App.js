ff2afdd83b79eef1b57b86336519961e788f87b9
yonghan <wangyh.92@gmail.com>
2021-04-17 18:19:19 +0800

    simple login testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,59 @@
 import './App.css';
+import {useState, useEffect} from 'react' 
 import Header from './components/Header'
 import Home from './components/Home'
+import NotFound from './components/NotFound'
+import {Container} from 'react-bootstrap'
 import AdminProfile from './components/AdminProfile'
 import {Switch,BrowserRouter as Router,Route,Redirect} from "react-router-dom";
 
 function App() {
+
+  const [isProfile, setIsProfile] = useState(false)
+
+  const loginHandler = (name) => {
+    if(name==="andy"){
+      setIsProfile(true);
+      // localStorage.setItem("token", "loggeeddddd");
+    }
+  }
+
+  // useEffect(()=>{
+  //   let isProfile = localStorage.getItem('token')
+  //   if(token!=='undefined'){
+  //     props.history.push('/')
+  //   }
+  // },[])
+
   return (
     <>
     <Router>
       <Header />
-        <div className="container">
+        <Container>
 
+        {/* login(name) */}
           <Switch>
             <Route 
               path="/"
               exact
-              component={Home}
+              // component={Home}
+              render={()=>
+                isProfile ? <Redirect to="/home"/> : <Home login={loginHandler}/>}
             />
-          </Switch>
         
-          <Switch>
+        {/* homepage */}
             <Route 
               path="/home"
               exact
-              component={AdminProfile}
+              render={()=>
+                isProfile ? <AdminProfile/> : <Redirect to="/"/>}
             />
+        
+        {/* Handle All other undefined path */}
+          <Route path='*' exact component={NotFound}/>
           </Switch>
-       
-        </div>
+          
+        </Container>
       </Router>
     </>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import {Form, Button} from 'react-bootstrap'
 
-export default function Home() {
+export default function Home(props) {
 
     const [value, setValue] = useState("");
 
     const handleChange = (event) => {
         setValue(event.target.value)
+    }
+
+    const loginHandler = () => {
+        props.login(value)
     }
 
     return (
@@ -16,7 +20,7 @@ export default function Home() {
                 <Form.Label>Name</Form.Label>
                 <Form.Control type="text" placeholder="Enter name" value={value} onChange={handleChange}/>
                 </Form.Group>
-                <Button variant="primary" type="submit" disabled={!value}>
+                <Button variant="primary" type="button" disabled={value.length<3} onClick={loginHandler}>
                     Submit
                 </Button>
             </Form>
